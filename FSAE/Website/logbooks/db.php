12ed55e11268325b4908aa92f8a7471d45065f6c
JesseRussell2240 <124098610+JesseRussell2240@users.noreply.github.com>
2025-05-20 13:30:17 -0400

    Update db.php

--- FSAE/Website/logbooks/db.php
+++ FSAE/Website/logbooks/db.php
@@ -1,4 +1,15 @@
 <?php
-$pdo = new PDO("mysql:host=localhost;dbname=logbooks", "siteuser", "sitepass123");
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$host = 'localhost';
+$db   = 'logbooks';
+$user = 'webuser';
+$pass = 'webpass';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+$pdo = new PDO($dsn, $user, $pass, $options);
 ?>
